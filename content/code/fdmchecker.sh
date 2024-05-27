0109df67083e0a6edd0abfda9df23fdddcaf66ee
Dr. David Krassnig <github@david-krassnig.8shield.net>
2024-05-27 19:21:10 +0200

    Almost finished draft of chapter 3

--- content/code/fdmchecker.sh
+++ content/code/fdmchecker.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-for FOLDER in * /; do
-    for FILE in "$FOLDER" *.pdf; do
-      echo "$FILE";
-      pdftotext "$FILE" - | grep -iE -C 5 --color=auto "forschungsdat|primärdat|sekundärdat|FDM|research data|praxis";
-      echo "-----------------";
-  done
-done
+for FOLDER in * /; do for FILE in "$FOLDER" *.pdf; do
+  echo "$FILE";
+  pdftotext "$FILE" - | grep -iE -C 5 --color=auto "forschungsdat|primärdat|sekundärdat|FDM|research data|praxis";
+  echo "-----------------";
+done; done
